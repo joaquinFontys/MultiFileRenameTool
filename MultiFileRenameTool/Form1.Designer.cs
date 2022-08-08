8b0ca0cad52c8fd4835215170c7cef03f9ae2126
joaquinFontys <j.thijssen@student.fontys.nl>
2022-08-08 17:26:51 +0200

    Basic Function update
    This update serves to establish the repo and adds the basic Idea of the app.
    - You can select a path
    - The selected path can be used to perform files changes on
    - a textbox has been added in which you can put a word to take out of the file names.

---
+++ MultiFileRenameTool/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace MultiFileRenameTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PathLbl = new System.Windows.Forms.Label();
+            this.SelectPathBtn = new System.Windows.Forms.Button();
+            this.WordToRemoveTxb = new System.Windows.Forms.TextBox();
+            this.SubmitPathBtn = new System.Windows.Forms.Button();
+            this.ErrorLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PathLbl
+            // 
+            this.PathLbl.AutoSize = true;
+            this.PathLbl.Location = new System.Drawing.Point(135, 139);
+            this.PathLbl.Name = "PathLbl";
+            this.PathLbl.Size = new System.Drawing.Size(74, 15);
+            this.PathLbl.TabIndex = 0;
+            this.PathLbl.Text = "Select a path";
+            // 
+            // SelectPathBtn
+            // 
+            this.SelectPathBtn.Location = new System.Drawing.Point(135, 113);
+            this.SelectPathBtn.Name = "SelectPathBtn";
+            this.SelectPathBtn.Size = new System.Drawing.Size(75, 23);
+            this.SelectPathBtn.TabIndex = 1;
+            this.SelectPathBtn.Text = "Select Path";
+            this.SelectPathBtn.UseVisualStyleBackColor = true;
+            this.SelectPathBtn.Click += new System.EventHandler(this.SelectPathBtn_Click);
+            // 
+            // WordToRemoveTxb
+            // 
+            this.WordToRemoveTxb.Location = new System.Drawing.Point(135, 157);
+            this.WordToRemoveTxb.Name = "WordToRemoveTxb";
+            this.WordToRemoveTxb.Size = new System.Drawing.Size(100, 23);
+            this.WordToRemoveTxb.TabIndex = 2;
+            // 
+            // SubmitPathBtn
+            // 
+            this.SubmitPathBtn.Location = new System.Drawing.Point(241, 156);
+            this.SubmitPathBtn.Name = "SubmitPathBtn";
+            this.SubmitPathBtn.Size = new System.Drawing.Size(75, 23);
+            this.SubmitPathBtn.TabIndex = 3;
+            this.SubmitPathBtn.Text = "Submit";
+            this.SubmitPathBtn.UseVisualStyleBackColor = true;
+            this.SubmitPathBtn.Click += new System.EventHandler(this.SubmitPathBtn_Click);
+            // 
+            // ErrorLbl
+            // 
+            this.ErrorLbl.AutoSize = true;
+            this.ErrorLbl.ForeColor = System.Drawing.Color.Red;
+            this.ErrorLbl.Location = new System.Drawing.Point(135, 183);
+            this.ErrorLbl.Name = "ErrorLbl";
+            this.ErrorLbl.Size = new System.Drawing.Size(0, 15);
+            this.ErrorLbl.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ErrorLbl);
+            this.Controls.Add(this.SubmitPathBtn);
+            this.Controls.Add(this.WordToRemoveTxb);
+            this.Controls.Add(this.SelectPathBtn);
+            this.Controls.Add(this.PathLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PathLbl;
+        private System.Windows.Forms.Button SelectPathBtn;
+        private System.Windows.Forms.TextBox WordToRemoveTxb;
+        private System.Windows.Forms.Button SubmitPathBtn;
+        private System.Windows.Forms.Label ErrorLbl;
+    }
+}
+
---
+++ MultiFileRenameTool/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiFileRenameTool
+{
+    public partial class Form1 : Form
+    {
+        String Path;
+        FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SelectPathBtn_Click(object sender, EventArgs e)
+        {
+            // Allows the user to sellect a path which then gets saved for later use
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Path = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+                PathLbl.Text = Path;
+            }
+        }
+
+        private void SubmitPathBtn_Click(object sender, EventArgs e)
+        {
+            ResetErrorLabel();
+            if (Check(PathLbl.Text))
+            {
+                DirectoryInfo d = new DirectoryInfo(@Path);
+                FileInfo[] infos = d.GetFiles();
+                try
+                {
+                    foreach (FileInfo f in infos)
+                    {
+                        File.Move(f.FullName, f.FullName.Replace(WordToRemoveTxb.Text, "")); // Careful!! This will replaces the text "abc_" anywhere in the path, including directory names.
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    ErrorLbl.Text += "The Folder does not contain a file with the text that you entered";
+                }
+                
+            }
+        }
+
+        private Boolean Check(String input)
+        {
+            // checks if the path that was selected was actually a valid path
+            if (input == "Select a path")
+            {
+                ErrorLbl.Text = "No path was selected";
+                return false;
+            }
+            return true;
+        }
+
+        private void ResetErrorLabel()
+        {
+            ErrorLbl.Text = "";
+        }
+    }
+}
